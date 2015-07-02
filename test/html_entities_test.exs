8da4fa5053b1058db545b8fb641270ff4f5eac50
Martin Svalin <martin@lite.nu>
2015-07-02 23:34:17 +0200

    Rely on doctest

--- test/html_entities_test.exs
+++ test/html_entities_test.exs
@@ -1,12 +1,4 @@
 defmodule HtmlEntitiesTest do
   use ExUnit.Case
-  import HtmlEntities
-
-  test "decode &amp;" do
-    assert decode("This &amp; that") == "This & that"
-  end
-
-  test "decode &#34;" do
-    assert decode("This &#38; that") == "This & that"
-  end
+  doctest HtmlEntities, import: true
 end
